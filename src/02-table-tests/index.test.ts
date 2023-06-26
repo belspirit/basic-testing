66a692cc9194dfa8ea2871a335dcff9ea397e934
Sergey <sergeylabetsik@datateer.com>
2023-06-26 12:15:27 -0500

    Implement 02 tests

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,37 @@
-// Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  { a: 1, b: 2, action: Action.Add, expected: 3 },
+  { a: -3, b: 1, action: Action.Add, expected: -2 },
+  { a: -4, b: -5, action: Action.Add, expected: -9 },
+  { a: 1, b: 2, action: Action.Substract, expected: -1 },
+  { a: -3, b: 1, action: Action.Substract, expected: -4 },
+  { a: -4, b: -5, action: Action.Substract, expected: 1 },
+  { a: 1, b: 2, action: Action.Multiply, expected: 2 },
+  { a: -3, b: 1, action: Action.Multiply, expected: -3 },
+  { a: -4, b: -5, action: Action.Multiply, expected: 20 },
+  { a: 1, b: 2, action: Action.Divide, expected: 0.5 },
+  { a: -3, b: 1, action: Action.Divide, expected: -3 },
+  { a: -20, b: -5, action: Action.Divide, expected: 4 },
+  { a: 1, b: 2, action: Action.Exponentiate, expected: 1 },
+  { a: -3, b: 1, action: Action.Exponentiate, expected: -3 },
+  { a: 4, b: -1, action: Action.Exponentiate, expected: 0.25 },
+  { a: 3, b: 3, action: Action.Exponentiate, expected: 27 },
+  { a: -3, b: 2, action: Action.Exponentiate, expected: 9 },
+  { a: 1, b: 2, action: 'badop', expected: null },
+  { a: 1, b: 2, action: '', expected: null },
+  { a: '1', b: 2, action: Action.Add, expected: null },
+  { a: 1, b: {}, action: Action.Add, expected: null },
+  { a: null, b: 2, action: Action.Add, expected: null },
+  { a: 1, b: undefined, action: Action.Add, expected: null },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
-  });
-  // Consider to use Jest table tests API to test all cases above
+  test.only.each(testCases)(
+    '$a $action $b = $expected',
+    ({ a, b, action, expected }) => {
+      let result = simpleCalculator({ a, b, action });
+      expect(result).toBe(expected);
+    },
+  );
 });
