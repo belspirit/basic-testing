bcecedc41b8149613c11727557a7a6af115cb6ae
Sergey <sergeylabetsik@datateer.com>
2023-06-27 12:47:06 -0500

    Mock module implementation instead of console log

--- src/05-partial-mocking/index.test.ts
+++ src/05-partial-mocking/index.test.ts
@@ -1,18 +1,29 @@
 import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
 
+jest.mock('./index', () => {
+  const originalModule =
+    jest.requireActual<typeof import('./index')>('./index');
+
+  return {
+    __esModule: true,
+    ...originalModule,
+    mockOne: jest.fn(),
+    mockTwo: jest.fn(),
+    mockThree: jest.fn(),
+  };
+});
+
 describe('partial mocking', () => {
   afterAll(() => {
     jest.unmock('./index');
   });
 
   test('mockOne, mockTwo, mockThree should not log into console', () => {
-    const logSpy = jest.spyOn(global.console, 'log').mockImplementation(() => {
-      // noop
-    });
+    const logSpy = jest.spyOn(global.console, 'log');
     mockOne();
     mockTwo();
     mockThree();
-    expect(logSpy).toBeCalledTimes(3);
+    expect(logSpy).not.toBeCalled();
     logSpy.mockRestore();
   });
 
