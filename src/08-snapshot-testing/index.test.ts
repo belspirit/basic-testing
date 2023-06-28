ff908186ed8a035078d88aaf4439b79537bc9e9a
Sergey <sergeylabetsik@datateer.com>
2023-06-28 08:38:39 -0500

    Implement 08 tests

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,32 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const res = generateLinkedList([1, 2, 3]);
+    expect(res).toStrictEqual({
+      next: { next: { next: { next: null, value: null }, value: 3 }, value: 2 },
+      value: 1,
+    });
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const res = generateLinkedList([1, 2, 3]);
+    expect(res).toMatchInlineSnapshot(`
+      {
+        "next": {
+          "next": {
+            "next": {
+              "next": null,
+              "value": null,
+            },
+            "value": 3,
+          },
+          "value": 2,
+        },
+        "value": 1,
+      }
+    `);
   });
 });
