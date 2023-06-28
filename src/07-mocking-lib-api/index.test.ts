d544bb171e7c4570b86372aa4e1e09dbdb956856
Sergey <sergeylabetsik@datateer.com>
2023-06-27 19:00:22 -0500

    Implement 07 tests

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,47 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
 
 describe('throttledGetDataFromApi', () => {
+  let getSpy: any;
+  const responseData = 'some data';
+
+  beforeEach(() => {
+    getSpy = jest
+      .spyOn(axios.Axios.prototype, 'get')
+      .mockImplementation(async () => ({ data: responseData }));
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+    getSpy.mockRestore();
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const createSpy = jest.spyOn(axios, 'create');
+    await throttledGetDataFromApi('some-path');
+    jest.runAllTimers();
+
+    const baseURLProvided = createSpy.mock.calls[0]?.[0]?.baseURL;
+    const { baseURL } = createSpy.mock.results[0]?.value.defaults;
+    expect(baseURLProvided).toBe(baseURL);
+    createSpy.mockRestore();
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const relativePath = 'some-relative-path';
+    await throttledGetDataFromApi(relativePath);
+    jest.runAllTimers();
+
+    const getRelativePath = getSpy.mock.calls[0]?.[0];
+    expect(getRelativePath).toBe(relativePath);
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const relativePath = 'some-relative-path';
+    const result = await throttledGetDataFromApi(relativePath);
+    jest.runAllTimers();
+
+    expect(result).toBe(responseData);
   });
 });
